b62ddabead0e19ec6aade51cf553a521e8596066
Stef van Houten <stefvanhouten@gmail.com>
2019-10-07 12:11:33 +0200

    'test'

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
+app.use(express.json());
+
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
+
+app.listen(process.env.SERVER_PORT, () =>
+  console.log("Server started on port: " + process.env.SERVER_PORT)
+);
